b1b47f61599d82c902a760eb01b3571ded3762a8
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-27 22:04:10 +0600

    scoll animation added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,63 +1,126 @@
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Link as ScrollLink,animateScroll } from "react-scroll";
+
 
 const NavBar = () => {
-  
-  
 
-    const navOption = (
-      <>
-        <li className="font-rubik">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="font-rubik">
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li className="font-rubik">
-          <Link to="/aboutMe">About Me</Link>
-        </li>
-        <li className="font-rubik">
-          <Link to="/contact">Contact</Link>
-        </li>
-        
-      </>
-    );
-    return (
-      <>
-        <div className="navbar fixed z-10 bg-opacity-50 w-full  bg-orange-400">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 shadow  bg-orange-500 bg-opacity-50 text-white  rounded-box w-52"
-              >
-                {navOption}
-              </ul>
-            </div>
-            <Link to="/" className="font-semibold ml-8 font-rubik text-xl">Alif Chowdhury Apurbo</Link>
-          </div>
-          <div className="navbar-center  hidden lg:flex lg:ml-44">
-            <ul className="menu menu-horizontal font-semibold px-1">{navOption}</ul>
-          </div>
-        </div>
-      </>
-    );
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const scrollToTop = () => {
+    animateScroll.scrollToTop();
+    closeDropdown();
+  };
+
+  const navOption = (
+    <>
+      <li className="font-rubik">
+        <ScrollLink
+          to="home"
+          smooth={true}
+          duration={500}
+          spy={true}
+          exact="true"
+          offset={70}
+          onClick={scrollToTop}
+        >
+          Home
+        </ScrollLink>
+      </li>
+      <li className="font-rubik">
+        <ScrollLink
+          to="aboutMe"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={-70}
+          onClick={closeDropdown}
+        >
+          About Me
+        </ScrollLink>
+      </li>
+      <li className="font-rubik">
+        <ScrollLink
+          to="projects"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={-80}
+          onClick={closeDropdown}
+        >
+          Projects
+        </ScrollLink>
+      </li>
+      <li className="font-rubik">
+        <ScrollLink
+          to="contact"
+          smooth={true}
+          duration={500}
+          spy={true}
+          offset={-70}
+          onClick={closeDropdown}
+        >
+          Contact
+        </ScrollLink>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar fixed z-10 bg-opacity-50 w-full  bg-orange-400">
+        <div className="navbar-start">
+        <div className="dropdown">
+          <label
+            tabIndex={0}
+            className="btn btn-ghost lg:hidden"
+            onClick={toggleDropdown}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          {isDropdownOpen && (
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow  bg-orange-500 bg-opacity-50 text-white  rounded-box w-52"
+              onClick={closeDropdown}
+            >
+              {navOption}
+            </ul>
+          )}
+        </div>
+
+          <Link to="/" className="font-semibold ml-8 font-rubik text-xl">
+            Alif Chowdhury Apurbo
+          </Link>
+        </div>
+        <div className="navbar-center  hidden lg:flex lg:ml-44">
+          <ul className="menu menu-horizontal font-semibold px-1">
+            {navOption}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NavBar;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,68 +1,87 @@
-import jobNavigator from '../../public/images/project images/project-home-image/job-navigator-home.jpg'
-import tastyMeal from '../../public/images/project images/project-home-image/tasty-meal-home.jpg'
-import enigmaMagic from '../../public/images/project images/project-home-image/enigma-home.jpg'
-import Modal from './Modal';
+import jobNavigator from "../../public/images/project images/project-home-image/job-navigator-home.jpg";
+import tastyMeal from "../../public/images/project images/project-home-image/tasty-meal-home.jpg";
+import enigmaMagic from "../../public/images/project images/project-home-image/enigma-home.jpg";
+import Modal from "./Modal";
+import { Fade } from "react-awesome-reveal";
 const Projects = () => {
   return (
-    <div>
+    <div id="projects">
       <h2 className="text-4xl font-bold ml-4 md:ml-20 mb-8  md:text-left  md:mb-4 font-rubik mt-28 md:mt-56">
         My Projects
       </h2>
-      <div className='grid md:grid-cols-3  md:mx-4'>
-        {/* Job navigator card */}
-        <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
-          <figure className="px-10 pt-10">
-            <img
-              src={jobNavigator}
-              alt="job navigator"
-              className="rounded-md"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Job Navigator</h2>
-            <p>Looking for Jobs ? this is the website for you.</p>
-            <div className="card-actions">
-              <button onClick={()=>window.my_modal_1.showModal()}  className="btn border-none text-white bg-orange-400 hover:bg-orange-200">See info</button>
+      <Fade delay={1e2} cascade damping={1e-1}>
+        <div className="grid md:grid-cols-3 md:-mt-8 md:mx-4">
+          {/* Job navigator card */}
+          <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src={jobNavigator}
+                alt="job navigator"
+                className="rounded-md"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Job Navigator</h2>
+              <p>Looking for Jobs ? this is the website for you.</p>
+              <div className="card-actions">
+                <button
+                  onClick={() => window.my_modal_1.showModal()}
+                  className="btn border-none text-white bg-orange-400 hover:bg-orange-200"
+                >
+                  See info
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-         {/* tasty meal card */}
-        <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
-          <figure className="px-10 pt-10">
-            <img
-              src={tastyMeal}
-              alt="job navigator"
-              className="rounded-md"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Job Navigator</h2>
-            <p>Looking for Jobs ? this is the website for you.</p>
-            <div className="card-actions">
-              <button onClick={()=>window.my_modal_2.showModal()} className="btn border-none text-white bg-orange-400 hover:bg-orange-200">See info</button>
-            </div>
-            
-          </div>
-        </div>
-        {/* enigma magic card */}
-        <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
-          <figure className="px-10 pt-10">
-            <img
-              src={enigmaMagic}
-              alt="job navigator"
-              className="rounded-md"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-            <h2 className="card-title">Job Navigator</h2>
-            <p>Looking for Jobs ? this is the website for you.</p>
-            <div className="card-actions">
-              <button onClick={()=>window.my_modal_3.showModal()}  className="btn border-none text-white bg-orange-400 hover:bg-orange-200">See info</button>
+          {/* tasty meal card */}
+          <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src={tastyMeal} alt="tasty meal" className="rounded-md" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Tasty meal</h2>
+              <p>
+                Are you Hungry ? Then visit Our Awesome Website and see many
+                recipes to your preferences.
+              </p>
+              <div className="card-actions">
+                <button
+                  onClick={() => window.my_modal_2.showModal()}
+                  className="btn border-none text-white bg-orange-400 hover:bg-orange-200"
+                >
+                  See info
+                </button>
+              </div>
             </div>
           </div>
+          {/* enigma magic card */}
+          <div className="card w-96 md:m-12 ml-2 mb-8 md:ml-4 bg-orange-300 font-rubik text-white shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src={enigmaMagic}
+                alt="Enigma Magic"
+                className="rounded-md"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">Enigma Magic</h2>
+              <p>
+                Looking for Classes To learn Magic ? this is the website for
+                you.
+              </p>
+              <div className="card-actions">
+                <button
+                  onClick={() => window.my_modal_3.showModal()}
+                  className="btn border-none text-white bg-orange-400 hover:bg-orange-200"
+                >
+                  See info
+                </button>
+              </div>
+            </div>
+          </div>
+          <Modal></Modal>
         </div>
-        <Modal></Modal>
-      </div>
+      </Fade>
     </div>
   );
 };
