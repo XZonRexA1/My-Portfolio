1904a4f49bb58de41fbaf0b7f20740b55cff2a3d
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-29 00:49:03 +0600

    footer margin issue fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="footer mx-4 footer-center font-rubik my-8 ">
+    <footer className="footer mx-4 md:mx-0 footer-center font-rubik my-8 ">
       <p>
         Copyright &copy; {currentYear} All rights reserved by
         <b className="text-orange-500">Alif Chowdhury Apurbo</b>
